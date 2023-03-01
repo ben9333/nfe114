c5b3b21206d35935eadad6ac56f5c4af4b7d5607
ben9333 <123626324+ben9333@users.noreply.github.com>
2023-03-01 12:12:29 +0100

    base

--- liste.php
+++ liste.php
@@ -11,7 +11,7 @@ $base = new PDO('mysql:host=localhost; dbname=id20205300_movies', 'id20205300_be
 $base->exec("SET CHARACTER SET utf8");
 
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT * FROM movies;');
+$retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
 
 //3° - Lecture du resultat de la requette
 while ($data = $retour->fetch()){
