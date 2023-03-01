4ade2702b496ea087de061632a3194045136ae61
ben9333 <123626324+ben9333@users.noreply.github.com>
2023-03-01 12:22:05 +0100

    base

--- recherche.php
+++ recherche.php
@@ -2,9 +2,14 @@
 <body>
 <h1>Recherche</h1>
 
+<?php
 
 //2° - Préparation de requette et execution
 $retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
+
+
+?>
+
 
 </body>
 </html>
