6af5f43b190407dcba7fa57b52b65d252513cf4f
ben9333 <123626324+ben9333@users.noreply.github.com>
2023-03-01 12:52:06 +0100

    ben

--- recherche.php
+++ recherche.php
@@ -2,13 +2,25 @@
 <body>
 <h1>recherche</h1>
 
+<p>ANNEE ? <SELECT name="annee">
 
 <?php
 
+//1° - Connexion à la BDD
+$base = new PDO('mysql:host=localhost; dbname=id20205300_movies', 'id20205300_beno', '+!v[QdLa4er?T1)I');
+$base->exec("SET CHARACTER SET utf8");
+
 //2° - Préparation de requette et execution
-$retour = $base->query('SELECT * FROM movies WHERE annee='.$ANNEE.';');
+$retour = $base->query('SELECT DISTINCT annee FROM movies;');
+
+//3° - Lecture du resultat de la requette
+while ($data = $retour->fetch()){
+echo "<option>".$data['annee']."</option>";
+}
 
 ?>
+</SELECT>
+</p>
 
 </body>
 </html>
